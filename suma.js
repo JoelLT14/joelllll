d400f9e05165ba14ca6fee0453b9e8d4c633efbc
JoelLT14 <115197233+JoelLT14@users.noreply.github.com>
2022-11-09 11:57:49 -0400

    Add files via upload

---
+++ suma.js
@@ -0,0 +1,51 @@
+//scrip para sumar//
+/*mas 
+lineas*/
+/*
+//definir variables//
+var a=5;
+let b=6;
+//sumar//
+var c=a+b;
+//mostrar resultado//
+document.write("<h2>la suma es: </h2>"+c)
+//pedir datos//
+
+let a=parseFloat(prompt("introdicir num1:"));
+let b=parseFloat(prompt("introdicir num2:"));
+//sumar//
+var c=a+b;
+//mostrar resultado//
+document.write("<h2 class=suma> la suma es: "+c+"</h2>")
+//restar//
+var r=a-b;
+//mostrar resultado//
+document.write("<h2 class=suma> la resta es: "+r+"</h2>")
+//multiplicar//
+var m=a*b;
+//mostrar resultado//
+document.write("<h2 class=suma> la multiplicacion es: "+m+"</h2>")
+//dividir//
+var d=a/b;
+//mostrar resultado//
+document.write("<h2 class=suma> la division es: "+d+"</h2>")
+//potencia//
+var q=Math.pow(a,b)
+var p=q.toFixed(2)
+//mostrar resultado//
+document.write("<h2 class=suma> la potencias es: "+p+"</h2>")*/
+//pedir datos//
+
+let a=parseFloat(prompt("introdicir un numero:"));
+//sin//
+var s=Math.sin(a)
+//mostrar resultado//
+document.write("<h2 class=suma> sin es: "+s+"</h2>")
+//cos//
+var c=Math.cos(a)
+//mostrar resultado//
+document.write("<h2 class=suma> cos es: "+c+"</h2>")
+//tan//
+var c=Math.tan(a)
+//mostrar resultado//
+document.write("<h2 class=suma> tan es: "+c+"</h2>")
